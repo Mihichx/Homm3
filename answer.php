db3e33ac02ceb75ad05c179b8e3d36b32310ed3b
Mi hich x <Misha777px@gmail.com>
2026-04-11 19:40:16 +0400

    Фикс моих ошибок

--- answer.php
+++ answer.php
@@ -23,6 +23,7 @@ $opt = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES   => true,
+    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
 ];
 
 $db = null;
